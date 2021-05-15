b53d7fb1f17e8ba2597df8ed87387602e87a8874
Alfred <76535763+free20064u@users.noreply.github.com>
2021-05-15 01:42:01 +0000

    program

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
     path('programs/', views.programs, name='programs'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('programs/<course>/', views.course , name='course')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -16,3 +16,11 @@ def programs(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+
+def course(request, course=None):
+    course = course
+    context = {
+        'course': course
+    }
+    return render(request, 'course.html', context)
